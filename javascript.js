1692d17801589fca61f51db75156d8679dd3c315
dtayurskiy <115502460+dtayurskiy@users.noreply.github.com>
2023-03-20 13:49:30 -0400

    Created playRound function, which determines a winner based on the  choices of the player and computer.

--- javascript.js
+++ javascript.js
@@ -4,10 +4,11 @@
 //Record who won
 //Once one player gets 3 wins, end the game and declare that player the winner
 
-let playerChoice = "ROCK";
+let playerChoice = "scIssoRs";
 playerChoice = playerChoice.toLowerCase();
 const computerChoice = getComputerChoice();
 
+//Have program randomly pick between rock, paper, or scissors
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
@@ -15,5 +16,25 @@ function getComputerChoice() {
     return finalChoice;
 }
 
-console.log(playerChoice)
-console.log(computerChoice)
+//Determine result of the game by comparing the selection of the player to that of the computer
+function playRound(player, computer) {
+    let result;
+    
+    if (player === computer) {
+        result = "It's a draw!"
+    }
+    else if (player === "rock" && computer === "paper" || player === "paper" && computer === "scissors" || player === "scissors" && computer === "rock") {
+        result = "Computer wins! " + computer + " beats " + player;
+    }
+    else if (computer === "rock" && player === "paper" || computer === "paper" && player === "scissors" || computer === "scissors" && player === "rock") {
+        result = "You win! " + player + " beats " + computer;
+    }
+    else {
+        result ="Invalid selection, please choose between rock, paper, or scissors";
+    }
+
+    return result;
+}
+
+console.log(playRound(playerChoice, computerChoice))
+
